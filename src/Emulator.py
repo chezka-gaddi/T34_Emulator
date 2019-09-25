1e90e88b89ef0d03f6a0fa945fc7bd2733731a82
Gaddi <7180120@linux16.cse.sdsmt.edu>
2019-09-25 15:48:14 -0600

    Loading program to memory.

--- src/Emulator.py
+++ src/Emulator.py
@@ -14,7 +14,7 @@ class Emulator:
             program_name
                 Name of the program to be loaded into the emulator
         """
-
+        self.memory = bytearray(65536)
         self.program = program_name
         if self.program is not None:
             self.load_program()
@@ -28,24 +28,43 @@ class Emulator:
             :return: successful read
 
             - Example::
-
                 load_program(a.c)
 
             - Expected Success Response::
-
                 True
 
             - Expected Fail Response::
-
                 False
 
         """
 
-    #    with open(self.program) as f:
-    #        lineList = f.readlines()
+        with open(self.program) as f:
+           lineList = f.readlines()
 
-    #    lineList = [line.rstrip('\n') for line in open(self.program)]
-        lineList = open(self.program, "rb").read()
+        # lineList = [line.rstrip('\n') for line in open(self.program)]
+        # lineList = open(self.program, "rb").read()
+
+        for line in lineList:
+            logger.debug(line)
+            bytecount = line[1:3]
+            bytecount = int(bytecount, 16)
+            address = line[3:7]
+            address = int(address, 16)
+            record_type = line[7:9]
+            data = line[9:-3]
+            data = str.encode(data)
+            checksum = line[-3:]
+
+            self.memory[address:address+bytecount] = data[:]
+            
+            logger.debug(self.memory[address:address+bytecount])
+            logger.debug(self.memory[address:address+1])
+
+            logger.debug("Bytecount: " + str(bytecount))
+            logger.debug("Address: " + str(address))
+            logger.debug("Record Type: " + record_type)
+            logger.debug(data)
+            logger.debug(checksum)
 
         logger.debug(lineList)
 
